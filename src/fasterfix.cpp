d3b03316f89f0759cfec32b2aa5f307e321cdac5
Roman Svihalek <svihalek@activitymon.com>
2023-06-20 20:14:19 +0200

    cmake

---
+++ src/fasterfix.cpp
@@ -0,0 +1,12 @@
+﻿// fasterfix.cpp : Defines the entry point for the application.
+//
+
+#include "fasterfix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
